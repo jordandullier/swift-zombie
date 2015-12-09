23ff21f5f18fad0100910dbf7b16f506aecba8c1
Jordan <jordan.dullier29@gmail.com>
2015-12-09 16:47:14 +0100

    add collision

--- ZombieEscape/ViewController.swift
+++ ZombieEscape/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var boardView: UIView!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var lifeLabel: UILabel!
     var player:Player?
-    var zombies:[Zombie] = []
+    var zombies:[Zombie]! = []
     var game:Game?
     var currentSpawnTime:Int? = 0
     
@@ -31,6 +32,7 @@ class ViewController: UIViewController {
         self.player = Player(health: 10, xPosition: 3, yPosition: 6, name: "myPlayer")
         self.game = Game(sizeX:12, sizeY: 16, startPlayerHealth: 4, spawnRate: 4)
         self.game!.calculateSizeCell(widthScreen, heigthScreen: heightScreen)
+        self.lifeLabel.text = String(player!.health!)
         player!.drawCharacter(boardView, game: self.game!)
         listenerGesture()
         
@@ -66,12 +68,13 @@ class ViewController: UIViewController {
             self.currentSpawnTime? += 1
             
             if( self.currentSpawnTime == game?.spawnRate){
-                 self.currentSpawnTime? = 0
+                self.currentSpawnTime? = 0
                 let zombie:Zombie = Zombie(xPosition: 10, yPosition: 2)
                 zombie.drawCharacter(boardView, game: game!)
+                self.zombies.append(zombie)
             }
             
-            print(self.currentSpawnTime)
+            
             
             scoreLabel.text = String(player!.score)
             switch swipeGesture.direction {
@@ -86,7 +89,38 @@ class ViewController: UIViewController {
             default:
                 break
             }
+            
+            var index = 0;
+            
+            for zm in zombies{
+                
+                
+                var randomNum:Int = randomNumber(1...4)
+                
+                switch(randomNum){
+                case 1: zm.moveCharacterDown(game!)
+                case 2: zm.moveCharacterRight(game!)
+                case 3: zm.moveCharacterUp(game!)
+                case 4: zm.moveCharacterLeft(game!)
+                default: break
+                }
+                
+                if(zm.xPosition == player?.xPosition && zm.yPosition == player?.yPosition){
+                    player?.health! -= 1
+                    self.lifeLabel.text = String(player!.health!)
+                    zm.subview.removeFromSuperview()
+                    zombies.removeAtIndex(index)
+                }
+                 index += 1;
+                
+            }
         }
+    }
+    
+    func randomNumber(range: Range<Int> = 1...6) -> Int {
+        let min = range.startIndex
+        let max = range.endIndex
+        return Int(arc4random_uniform(UInt32(max - min))) + min
     }
 
 
